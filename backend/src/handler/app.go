3f92f456fd52ae852633efa6092e6398b05f728a
zaczeng16 <zaczeng16@gmali.com>
2024-05-01 03:45:14 +0000

    upload handler

--- backend/src/handler/app.go
+++ backend/src/handler/app.go
@@ -1,1 +1,22 @@
 package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"app-bazaar/model"
+)
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received a upload request")
+	decoder := json.NewDecoder(r.Body)
+	var app model.App
+	if err := decoder.Decode(&app); err != nil {
+		panic(err)
+	}
+	_, err := fmt.Fprintf(w, "Upload request received: %s\n", app.Description)
+	if err != nil {
+		return
+	}
+}
